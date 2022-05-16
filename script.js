f316da6596e01d5ce0ee47422f445ad139839063
KenzoVH <kenzovanhaesebroeck@gmail.com>
2022-05-16 12:58:33 -0400

    Fixed script link, added computerPlay function

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+var choices = ["Rock", "Paper", "Scissors"];
+
+// computerPlay => choices[Math.floor(Math.random() * choices.length)];
+// Arrow function was not callable, kept in comment as a future reference
+
+function computerPlay() {
+    // Generate a random index from the choices, return this
+    return choices[Math.floor(Math.random() * choices.length)];
+}
 
