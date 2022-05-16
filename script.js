5077396a878ba3bfe715874a5f8223ce1cfb6680
KenzoVH <kenzovanhaesebroeck@gmail.com>
2022-05-16 15:02:25 -0400

    Added playRound function

--- script.js
+++ script.js
@@ -1,10 +1,45 @@
-var choices = ["Rock", "Paper", "Scissors"];
+const choices = ["rock", "scissors", "paper"];
 
 // computerPlay => choices[Math.floor(Math.random() * choices.length)];
 // Arrow function was not callable, kept in comment as a future reference
 
 function computerPlay() {
-    // Generate a random index from the choices, return this
+    // Generate a random index from the choices
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
+function playRound(playerSelection, computerSelection) {
+    // compare the two selections, if computer wins, return player loses, if player wins, return computer loses
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    let result;
+    let winner;
+    let loser;
+
+    if (playerSelection === computerSelection) {
+        result = "tie"
+        winner = capitalize(playerSelection);
+        loser = capitalize(computerSelection);
+        return `You ${result}, ${winner} ties with ${loser}!`
+    } else if (
+        playerSelection === "rock" && computerSelection === "scissors" ||
+        playerSelection === "scissors" && computerSelection === "paper" ||
+        playerSelection === "paper" && computerSelection === "rock"
+        ) {
+        result = "win";
+        winner = capitalize(playerSelection);
+        loser = capitalize(computerSelection);
+    } else {
+        result = "lose";
+        winner = capitalize(computerSelection);
+        loser = capitalize(playerSelection);
+    }
+    return `You ${result}, ${winner} beats ${loser}!`
+}
+
+function capitalize(word) {
+    // Function to capitalize words
+    let firstLetter = word.toLowerCase().substring(0, 1)
+    return firstLetter.toUpperCase() + word.substring(1);
+}
