dc97d4db43079272bef23f606fde7cca4dcea623
KenzoVH <kenzovanhaesebroeck@gmail.com>
2022-05-19 11:30:37 -0400

    Added buttons and their eventlisteners

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
+// Array with possible choices
 const choices = ["rock", "scissors", "paper"];
 
-// computerPlay => choices[Math.floor(Math.random() * choices.length)];
-// Arrow function was not callable, kept in comment as a future reference
-
+// Function to generate a random choice for the computer
 function computerPlay() {
     // Generate a random index from the choices
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
+// Function to play one round
 function playRound(playerSelection, computerSelection) {
     // compare the two selections, if computer wins, return player loses, if player wins, return computer loses
     playerSelection = playerSelection.toLowerCase();
@@ -32,26 +32,37 @@ function playRound(playerSelection, computerSelection) {
     return result;
 }
 
-function game() {
-    let round = 1;
-    let playerScore = 0;
-    let computerScore = 0;
-    let winner;
-    for (round; round <= 5; round++) {
-        playerSelection = prompt("Rock, paper or scissors?");
-        result = playRound(playerSelection, computerPlay());
-        if (result === "win") {
-            playerScore++;
-        } else if (result === "lose") {
-            computerScore++;
-        }
-    }
-    playerScore > computerScore ? winner = "Player" : winner = "Computer";
-    console.log(`The ${winner} wins! Final score: Player: ${playerScore}; Computer: ${computerScore}`);
-}
-
+// Function to capitalize words
 function capitalize(word) {
-    // Function to capitalize words
     let firstLetter = word.toLowerCase().substring(0, 1)
     return firstLetter.toUpperCase() + word.substring(1);
 }
+
+// Adding event listeners for buttons:
+const buttons = document.querySelectorAll('#btn');
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        let computerSelection = computerPlay();
+        let playerSelection = button.getAttribute('data-key');
+        console.log(`Player: ${playerSelection}, Computer: ${computerSelection}`);
+        playRound(playerSelection, computerSelection);
+    });
+});
+
+// function game() {
+//     let round = 1;
+//     let playerScore = 0;
+//     let computerScore = 0;
+//     let winner;
+//     for (round; round <= 5; round++) {
+//         playerSelection = prompt("Rock, paper or scissors?");
+//         result = playRound(playerSelection, computerPlay());
+//         if (result === "win") {
+//             playerScore++;
+//         } else if (result === "lose") {
+//             computerScore++;
+//         }
+//     }
+//     playerScore > computerScore ? winner = "Player" : winner = "Computer";
+//     console.log(`The ${winner} wins! Final score: Player: ${playerScore}; Computer: ${computerScore}`);
+// }
